82cfd69708a7816cd39f1db71d8c18c45333d6f2
Berry <a.berardi@evostudios.it>
2020-10-04 00:28:21 +0200

    Started working on mobile menu

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,31 +1,31 @@
 <template>
     <header id="page__header" class="fixed top-0 left-0 w-full h:8 lg:h-16 px-4 py-2 lg:py-0 flex items-center justify-between bg-purple2 text-white text-sm">
         <div class="flex items-center">
-            <div class="header__logo mr-8">
+            <div class="header__logo mr-8 relative z-40">
                 <img src="~/assets/images/berry_twitter.jpg" alt="Berry Image" class="rounded-full w-10 lg:w-12">
             </div>
-            <div class="header__nav">
-                <ul class="flex">
-                    <li class="px-4">
+            <nav id="main-menu" class="header__nav fixed inset-0 lg:static flex items-center bg-black lg:bg-transparent z-40">
+                <ul class="lg:flex w-full text-center lg:text-left text-lg lg:text-base">
+                    <li class="py-2 lg:py-0 px-4">
                         <nuxt-link to="/">
                             Home
                         </nuxt-link>
                     </li>
-                    <li class="px-4">
+                    <li class="py-2 lg:py-0 px-4">
                         <nuxt-link to="/skills">
                             Skills
                         </nuxt-link>
                     </li>
-                    <li class="px-4">
+                    <li class="py-2 lg:py-0 px-4">
                         <nuxt-link to="/styleguide">
                             Style Guide
                         </nuxt-link>
                     </li>
                 </ul>
-            </div>
+            </nav>
         </div>
 
-        <div id="page-header-socials" class="header__socials flex">
+        <div id="page-header-socials" class="header__socials flex relative z-40">
             <div class="social social--twitter w-6 mr-4">
                 <a :href="data.socialsLinks.twitter" target="_blank"><twitterIcon /></a>
             </div>
@@ -54,3 +54,12 @@ export default {
     }
 }
 </script>
+
+<style>
+    .social svg{
+        fill: theme('colors.white');
+    }
+    .social svg:hover{
+        fill: theme('colors.lime');
+    }
+</style>
