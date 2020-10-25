e3aeab20f30904a1165f946aa527b606016e68c3
Berry <a.berardi@evostudios.it>
2020-10-25 16:46:47 +0100

    Layout: sticky footer and centered content if not enought height to fill screen height

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-    <div id="page">
+    <div id="page" class="flex flex-col min-h-screen">
         <PageHeader />
 
-        <main id="main" class="min-h-screen bg-black text-graylight py-32 lg:py-48 px-4">
+        <main id="main" class="bg-black text-graylight pt-16 pb-12 xl:pt-48 xl:pb-12 lg:pt-16 lg:pb-0 px-4 overflow-hidden flex-grow md:flex md:items-center">
             <Nuxt />
         </main>
 
