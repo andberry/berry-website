0b20aef5a03c8ae91b22a0b0586407987cc3ff9c
Berry <a.berardi@evostudios.it>
2020-10-25 08:37:35 +0100

    Implemented mobileMenu openClose animation

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -3,13 +3,13 @@
     <header id="page__header" class="fixed top-0 left-0 w-full h:8 lg:h-16 px-4 py-2 lg:py-0 flex items-center justify-between bg-purple text-white text-sm z-40">
         <div class="flex items-center">
 
-            <div class="header__logo lg:mr-4 relative z-40">
+            <div class="header__logo lg:mr-4 relative z-40 w-10 lg:w-12">
                 <nuxt-link to="/">
-                    <img src="~/assets/images/berry_twitter.jpg" alt="Berry Image" class="rounded-full w-10 lg:w-12">
+                    <img src="~/assets/images/berry_twitter.jpg" alt="Berry Image" class="rounded-full">
                 </nuxt-link>
             </div>
 
-            <nav id="main-menu" class="header__nav mobile-menu--hidden fixed inset-0 lg:static flex items-center bg-black lg:bg-transparent z-40">
+            <nav id="main-menu" class="header__nav fixed inset-0 lg:static flex items-center bg-black lg:bg-transparent z-40">
                 <ul class="lg:flex w-full text-center lg:text-left text-lg lg:text-base">
                     <li class="py-2 lg:py-0 px-4">
                         <nuxt-link to="/" class="text-3xl md:text-4xl lg:text-base">
@@ -45,13 +45,16 @@
                 </div>
             </div>
 
-            <div class="mobile-menu-toggle block lg:hidden">
-                <button v-on:click="toggleMobileMenu" class="w-8"><hamburgerIcon /></button>
+            <div class="block w-8 mt-1 lg:hidden">
+                <div class="mobile-menu-toggle">
+                    <button v-on:click="toggleMobileMenu"><hamburgerIcon /></button>
+                </div>
             </div>
         </div>
     </header>
 </template>
 <script>
+import { gsap } from 'gsap'
 import data from '~/assets/data/data.json'
 import twitterIcon from '~/assets/images/twitter1.svg?inline'
 import linkedinIcon from '~/assets/images/linkedin.svg?inline'
@@ -68,25 +71,51 @@ export default {
 
     data () {
         return {
-            data
+            data,
+            isMobileMenuOpen: false,
+            mobileMenuAnimation: {},
+            openMobileMenu: {},
+            closeMobileMenu: {}
         }
     },
 
     methods: {
         toggleMobileMenu () {
-            const menuEl = document.getElementById('main-menu')
-            menuEl.classList.toggle('mobile-menu--hidden')
+            if (!this.isMobileMenuOpen) {
+                this.openMobileMenu.restart()
+            } else {
+                this.closeMobileMenu.restart()
+            }
+            this.isMobileMenuOpen = !this.isMobileMenuOpen
         }
     },
 
     mounted () {
-        // Close mobile-menu after using a link in the menu
+        // Setup open mobile menu animation
         const menuEl = document.getElementById('main-menu')
+        const menuItemsEls = document.querySelectorAll('#main-menu li')
+        const openMobileMenuTl = gsap.timeline({ paused: true, defaults: { duration: 0.4, ease: 'power4.out' } })
+
+        openMobileMenuTl.to(menuEl, { autoAlpha: 1, duration: 0.2, ease: 'none' })
+        openMobileMenuTl.fromTo(
+            menuItemsEls,
+            { opacity: 0, x: -15 },
+            { opacity: 1, x: 0, stagger: 0.1 },
+            '>-0.1'
+        )
+        this.openMobileMenu = openMobileMenuTl
+
+        // Setup close mobile menu animation
+        const closeMobileMenuTl = gsap.timeline({ paused: true, defaults: { duration: 0.4, ease: 'power4.in' } })
+        closeMobileMenuTl.to(menuItemsEls, { opacity: 0, x: 30, stagger: 0.1 })
+        closeMobileMenuTl.to(menuEl, { autoAlpha: 0, duration: 0.2, ease: 'none' }, '>-0.1')
+        this.closeMobileMenu = closeMobileMenuTl
+
+        // Close mobile-menu after using a link in the menu
         const menuLinksEls = document.querySelectorAll('#main-menu a')
         for (const item of Array.from(menuLinksEls)) {
             item.addEventListener('click', () => {
-                console.log('close mobile menu')
-                menuEl.classList.add('mobile-menu--hidden')
+                this.closeMobileMenu.restart()
             })
         }
     }
@@ -103,20 +132,7 @@ export default {
     }
 
     #main-menu{
-        @apply transition-all duration-200 ease-linear;
-
-        @screen lg {
-            @apply transition-none;
-        }
-    }
-
-    .mobile-menu--hidden{
         opacity: 0;
         visibility: hidden;
-
-        @screen lg {
-            opacity: 1;
-            visibility: visible;
-        }
     }
 </style>
