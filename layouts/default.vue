1062329a9d0d387d270226fa7d9979577f9c6853
Berry <a.berardi@evostudios.it>
2020-10-25 19:15:01 +0100

    paddings cleanup on default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div id="page" class="flex flex-col min-h-screen">
         <PageHeader />
 
-        <main id="main" class="bg-black text-graylight pt-16 pb-12 xl:pt-48 xl:pb-12 lg:pt-16 lg:pb-0 px-4 overflow-hidden flex-grow md:flex md:items-center">
+        <main id="main" class="bg-black text-graylight pt-16 pb-12 lg:pb-0 px-4 overflow-hidden flex-grow md:flex md:items-center">
             <Nuxt />
         </main>
 
