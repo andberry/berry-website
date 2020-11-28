523547a490f836d386aa327ec7a17562ecd159b4
Berry <a.berardi@evostudios.it>
2020-11-28 11:07:29 +0100

    Footer: added nuxt-content link and small change on style

--- components/PageFooter.vue
+++ components/PageFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer id="page__footer" :class="[{ fixed: isSticky, 'bottom-0': isSticky, 'left-0': isSticky}, 'w-full', 'h-10', 'px-4' ,'flex', 'items-center', 'bg-black2', 'text-gray', 'text-xs', 'flex-shrink-0']">
         <p class="mb-0">
-            Proudly built with <a :href="data.footerLinks.nuxt" target="_blank">Nuxt.js</a> and <a :href="data.footerLinks.tailwind" target="_blank">TailwindCSS</a>
+            Proudly built with <a :href="data.footerLinks.nuxt" target="_blank">Nuxt.js</a>, <a :href="data.footerLinks.nuxtcontent" target="_blank">@nuxt/content</a> and <a :href="data.footerLinks.tailwind" target="_blank">TailwindCSS</a>
         </p>
     </footer>
 </template>
